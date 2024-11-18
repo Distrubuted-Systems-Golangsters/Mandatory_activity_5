285ab6b976643de587311186cef6b52507227d15
mhiv <mhiv@itu.dk>
2024-11-18 20:14:00 +0100

    Add base auction functionality

--- client/main.go
+++ client/main.go
@@ -1,1 +1,119 @@
 package main
+
+import (
+	pb "auction/grpc"
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var bidderName string
+
+func main() {
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	conn, err := grpc.NewClient("localhost:8080", opts...)
+
+	if err != nil {
+		log.Fatalf("Connection failed: %v\n", err)
+	}
+
+	client := pb.NewAuctionServiceClient(conn)
+	startApp(client)
+}
+
+func placeBid(auctionName string, amount int, client pb.AuctionServiceClient) (string, error) {
+	res, err := client.Bid(context.Background(), &pb.BidRequest{ AuctionName: auctionName, BidderName: bidderName, Amount: int32(amount), Timestamp: 0 })
+
+	if err != nil {
+		return "", err
+	}
+
+	var message string
+	if res.Status == "Success" {
+		message = fmt.Sprintf("Bid successful. You are now the highest bidder on the auction %s", auctionName)
+	} else {
+		message = "Bid failed"
+	}
+
+	return message, err
+}
+
+func getAuctionStatus(auctionName string, client pb.AuctionServiceClient) (string, error) {
+	res, err := client.Result(context.Background(), &pb.ResultRequest{ AuctionName: auctionName, Timestamp: 0 })
+
+	if err != nil {
+		return "", err
+	}
+
+	var message string
+	if res.Status == "Ongoing" {
+		message = fmt.Sprintf("Auction is ongoing. Highest bid is %d by %s", res.HighestBid, res.BidderName)
+	} else {
+		message = fmt.Sprintf("Auction has ended. Highest bid was %d by %s", res.HighestBid, res.BidderName)
+	}
+
+	return message, err
+}
+
+func getBidderName() string {
+	fmt.Printf("Enter your name: ")
+	reader := bufio.NewReader(os.Stdin)
+	enteredString, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("Failed to read from console: %v\n", err)
+	}
+	enteredString = strings.Trim(enteredString, "\r\n")
+
+	return enteredString
+}
+
+func startApp(client pb.AuctionServiceClient) {
+	bidderName = getBidderName()
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		enteredString, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatalf("Failed to read from console: %v\n", err)
+		}
+		enteredString = strings.ToLower(strings.Trim(enteredString, "\r\n"))
+
+		auctionName := strings.Split(enteredString, " ")[0]
+		secondEnter := strings.Split(enteredString, " ")[1]
+
+		if secondEnter == "status" {
+			message, err := getAuctionStatus(auctionName, client)
+
+			if err != nil {
+				log.Fatalf("Failed to get auction status: %v\n", err)
+				continue
+			}
+
+			log.Println(message)
+			continue
+		}
+
+		bid, err := strconv.Atoi(secondEnter)
+		if err != nil {
+			fmt.Println("Error converting string to integer:", err)
+		}
+
+		message, err := placeBid(auctionName, bid, client)
+
+		if err != nil {
+			log.Fatalf("Failed to bid: %v\n", err)
+			continue
+		}
+
+		log.Println(message)
+	}
+}
--- server/main.go
+++ server/main.go
@@ -1,1 +1,97 @@
 package main
+
+import (
+	pb "auction/grpc"
+	"bufio"
+	"context"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	pb.UnimplementedAuctionServiceServer
+}
+
+type Auction struct {
+	name string
+	highestBid int32
+	bidderName string
+	endTime time.Time
+}
+
+var auctionTimeLength = 30
+var auctions = make(map[string]*Auction)
+
+func main() {
+	go startListening()
+	auctionPrompt()
+}
+
+func (s *Server) Bid(ctx context.Context, in *pb.BidRequest) (*pb.BidResponse, error) {
+	status := "Fail"
+	auction := auctions[in.AuctionName]
+	
+	if(auction.endTime.After(time.Now()) && in.Amount > auction.highestBid) {
+		auction.highestBid = in.Amount
+		auction.bidderName = in.BidderName
+		status = "Success"
+	}
+
+	return &pb.BidResponse{ Status: status, Timestamp: 0 }, nil
+}
+
+func (s *Server) Result(ctx context.Context, in *pb.ResultRequest) (*pb.ResultResponse, error) {
+	status := "Ongoing"
+	auction := auctions[in.AuctionName]
+
+	if(auction.endTime.Before(time.Now())) {
+		status = "Ended"
+	}
+
+	return &pb.ResultResponse{ Status: status, HighestBid: auction.highestBid, BidderName: auction.bidderName, Timestamp: 0 }, nil
+}
+
+func startListening() {
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Server listening on %v\n", lis.Addr())
+
+	s := grpc.NewServer()
+	pb.RegisterAuctionServiceServer(s, &Server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v\n", err)
+	}
+}
+
+func startAuction() {
+	reader := bufio.NewReader(os.Stdin)
+	enteredString, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("Failed to read from console: %v\n", err)
+	}
+	enteredString = strings.ToLower(strings.Trim(enteredString, "\r\n"))
+
+	auctions[enteredString] = &Auction{ 
+		name: enteredString, 
+		highestBid: 0, 
+		bidderName: "No bidder",  
+		endTime: time.Now().Add(time.Duration(auctionTimeLength) * time.Second),
+	}
+
+	log.Printf("Auction %v started. Auction will end in %d seconds\n", enteredString, auctionTimeLength)
+}
+
+func auctionPrompt() {
+	for {
+		startAuction()
+	}
+}
